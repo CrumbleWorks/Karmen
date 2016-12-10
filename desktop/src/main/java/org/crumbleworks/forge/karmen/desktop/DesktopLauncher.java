9e5a0e0c918e0fba53b860adcae95e4b8bdf0102
McDonnough <psbaechli+atlassian@gmail.com>
2016-12-10 08:45:51 +0100

    initial commit

---
+++ desktop/src/main/java/org/crumbleworks/forge/karmen/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.crumbleworks.forge.karmen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.crumbleworks.forge.karmen.Karmen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Karmen(), config);
+	}
+}
