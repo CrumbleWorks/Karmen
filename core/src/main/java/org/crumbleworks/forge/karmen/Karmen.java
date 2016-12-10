6977b7e5bf576bba4b3e0c19a0f7a8549b2ceb56
McDonnough <psbaechli+atlassian@gmail.com>
2016-12-10 10:21:21 +0100

    added intro screen

--- core/src/main/java/org/crumbleworks/forge/karmen/Karmen.java
+++ core/src/main/java/org/crumbleworks/forge/karmen/Karmen.java
@@ -1,33 +1,54 @@
 package org.crumbleworks.forge.karmen;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import org.crumbleworks.forge.karmen.screen.IntroScreen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Karmen extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Karmen extends Game {
+	
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 480;
+	
+	private SpriteBatch batch;
+	private BitmapFont font;
+	
+	private Screen introScreen;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		introScreen = new IntroScreen(this);
+		font = new BitmapFont();
+		
+		setScreen(introScreen);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+		
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public BitmapFont getFont() {
+        return font;
+    }
+	
 }
