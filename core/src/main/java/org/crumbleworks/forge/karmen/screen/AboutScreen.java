e1e07c20bab4472353d0c0d1f5a9c648dd4a541a
McDonnough <psbaechli+atlassian@gmail.com>
2016-12-10 23:42:04 +0100

    added scrolling credits

--- core/src/main/java/org/crumbleworks/forge/karmen/screen/AboutScreen.java
+++ core/src/main/java/org/crumbleworks/forge/karmen/screen/AboutScreen.java
@@ -1,31 +1,50 @@
 package org.crumbleworks.forge.karmen.screen;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.crumbleworks.forge.karmen.Karmen;
-import org.crumbleworks.forge.karmen.screen.MenuScreen.MenuButton;
 import org.crumbleworks.forge.karmen.util.NeonColors;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.Align;
 
 public class AboutScreen implements Screen {
 
     private final Karmen game;
     
+    private BitmapFont arialFont;
+    
+    private List<String> creditLines;
+    private int currentTopCreditLineIndex;
+    private int creditLinesOffset;
+    private float relatvieCreditPosition;
+    private float creditSpeed;
+    
     public AboutScreen(final Karmen game) {
         this.game = game;
+        
+        arialFont = new BitmapFont();
+        
+        currentTopCreditLineIndex = 0;
+        creditLinesOffset = 30;
+        relatvieCreditPosition = 0f;
+        creditSpeed = 0.25f;
+        
+        readAboutFile("about/NNPH.about");
+    }
+    
+    private void readAboutFile(String aboutFileName) {
+        creditLines = new ArrayList<>(Arrays.asList(Gdx.files.internal(aboutFileName).readString().split("\\r?\\n")));
     }
 
     @Override
-    public void show() {
-        // TODO Auto-generated method stub
-
-    }
+    public void show() {}
 
     @Override
     public void render(float delta) {
@@ -33,8 +52,28 @@ public class AboutScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
         game.getBatch().begin();
-        game.getFontMedium().setColor(NeonColors.Orange);
-        game.getFontMedium().draw(game.getBatch(), Gdx.files.internal("about/NNPH.about").readString(), 50, Karmen.SCREEN_HEIGHT - 100);
+        
+        arialFont.setColor(NeonColors.Orange);
+        
+        if(currentTopCreditLineIndex < creditLines.size()) {
+            currentTopCreditLineIndex = (int) (relatvieCreditPosition / creditLinesOffset);
+            if(currentTopCreditLineIndex >= creditLines.size()) {
+                currentTopCreditLineIndex = creditLines.size() - 1;
+            }
+        }
+        else {
+            currentTopCreditLineIndex = creditLines.size() - 1;
+        }
+        
+        relatvieCreditPosition += creditSpeed;
+        
+        for(int i = 0; i <= currentTopCreditLineIndex; i++) {
+            float fontX = Karmen.SCREEN_WIDTH / 4;
+            float fontY = relatvieCreditPosition - (i * creditLinesOffset);
+            
+            arialFont.draw(game.getBatch(), creditLines.get(i), fontX, fontY, Karmen.SCREEN_WIDTH / 2, Align.center, true);
+        }
+        
         game.getBatch().end();
         
         checkInput();
@@ -48,33 +87,20 @@ public class AboutScreen implements Screen {
     }
 
     @Override
-    public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-
-    }
+    public void resize(int width, int height) {}
 
     @Override
-    public void pause() {
-        // TODO Auto-generated method stub
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        // TODO Auto-generated method stub
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-        // TODO Auto-generated method stub
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
-        // TODO Auto-generated method stub
-
+        
     }
 
 }
