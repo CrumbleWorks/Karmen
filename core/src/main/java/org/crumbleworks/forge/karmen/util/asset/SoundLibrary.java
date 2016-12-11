123d50760c8ede703899e4cb98d12fc9f7c4e9e8
McDonnough <psbaechli+atlassian@gmail.com>
2016-12-11 18:14:28 +0100

    bla sound

--- core/src/main/java/org/crumbleworks/forge/karmen/util/asset/SoundLibrary.java
+++ core/src/main/java/org/crumbleworks/forge/karmen/util/asset/SoundLibrary.java
@@ -12,26 +12,37 @@ public class SoundLibrary {
     private Random random;
 
     private List<Sound> fistSounds;
+    private Sound jumpSound;
     
     public SoundLibrary() {
         random = new Random();
         
         initFistSounds("sfx/fist/Fist_1.wav", "sfx/fist/Fist_2.wav", "sfx/fist/Fist_3.wav");
+        jumpSound = loadSound("sfx/jump/Jump.wav");
+    }
+    
+    private Sound loadSound(String soundFileName) {
+        return Gdx.audio.newSound(Gdx.files.internal(soundFileName));
     }
     
     private void initFistSounds(String... soundFileNames) {
         fistSounds = new ArrayList<>(soundFileNames.length);
         for(String soundFileName : soundFileNames) {
-            fistSounds.add(Gdx.audio.newSound(Gdx.files.internal(soundFileName)));
+            fistSounds.add(loadSound(soundFileName));
         }
     }
     
     public Sound getRandomFistSound() {
         return fistSounds.get(random.nextInt(fistSounds.size()));
     }
+    
+    public Sound getJumpSound() {
+        return jumpSound;
+    }
 
     public void dispose() {
         fistSounds.forEach(s -> s.dispose());
+        jumpSound.dispose();
     }
     
 }
